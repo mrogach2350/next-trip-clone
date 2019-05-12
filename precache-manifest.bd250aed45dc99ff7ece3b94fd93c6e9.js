001dd4bc9ea90e485f9ad4c59c72446e7bc72c66
Michael Rogachevsky <m.rogach@gmail.com>
2019-05-11 22:23:32 -0500

    Updates

---
+++ precache-manifest.bd250aed45dc99ff7ece3b94fd93c6e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e020c2a09bd301f0656d51e8ef996cd",
+    "url": "/next-trip-clone/index.html"
+  },
+  {
+    "revision": "18acd53c6b7addac7755",
+    "url": "/next-trip-clone/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3f27ef9c65740759c349",
+    "url": "/next-trip-clone/static/js/2.4c07c699.chunk.js"
+  },
+  {
+    "revision": "18acd53c6b7addac7755",
+    "url": "/next-trip-clone/static/js/main.e4d61f2e.chunk.js"
+  },
+  {
+    "revision": "2afd389606fc83b7afc8",
+    "url": "/next-trip-clone/static/js/runtime~main.1ec4fbc7.js"
+  }
+]);
